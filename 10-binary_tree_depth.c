6d57be2b4a354c09ba0b1416000cae2e322a6c03
Aurylias <brycevermorel@gmail.com>
2026-08-03 16:02:43 +0200

    Change task 10 and add task 11, update the header

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -11,6 +11,6 @@
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	if (tree && tree->parent)
-		return (1 + binary_tree_depth(tree->parent));
+		return (binary_tree_depth(tree->parent) + 1);
 	return (0);
 }
---
+++ 11-binary_tree_size.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_size - Measures the size of a binary tree
+*
+* @tree: A pointer to the root of a binary tree
+*
+* Return: The size of the tree or 0 if the tree is NULL
+*/
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t size = 0;
+
+	if (!tree)
+		return (0);
+
+	size += binary_tree_size(tree->left) + 1;
+	size += binary_tree_size(tree->right) + 1;
+
+
+	return (size);
+}
