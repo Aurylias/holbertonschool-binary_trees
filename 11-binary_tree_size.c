6a4ef17a3d14e198528affc88d9116370943d9ce
Aurylias <brycevermorel@gmail.com>
2026-08-03 16:05:31 +0200

    Change task 11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -10,14 +10,16 @@
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t size = 0;
+	size_t left_size, right_size;
 
 	if (!tree)
 		return (0);
 
-	size += binary_tree_size(tree->left) + 1;
-	size += binary_tree_size(tree->right) + 1;
+	if (!tree->left && !tree->right)
+		return (1);
 
+	left_size = binary_tree_size(tree->left);
+	right_size = binary_tree_size(tree->right);
 
-	return (size);
+	return (left_size + right_size + 1);
 }
