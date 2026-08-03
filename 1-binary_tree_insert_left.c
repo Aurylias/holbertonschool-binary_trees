1cddff518c2b29f8900b9e92ea520e73447e6b5a
Aurylias <brycevermorel@gmail.com>
2026-08-03 12:50:01 +0200

    Change 1 to use last implemented function

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -16,14 +16,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (!parent)
 		return (NULL);
 
-	new = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+	new = binary_tree_node(parent, value);
 	if (!new)
 		return (NULL);
-
-	new->n = value;
-	new->parent = parent;
-	new->left = NULL;
-	new->right = NULL;
 
 	if (!parent->left)
 	{
