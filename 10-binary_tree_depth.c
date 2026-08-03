5184b32e3c2da127dd7504335594247955b344c3
Aurylias <brycevermorel@gmail.com>
2026-08-03 15:47:29 +0200

    Fixed wrong function call l18

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -15,7 +15,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	depth = binary_tree_height(tree->parent) + 1;
+	depth = binary_tree_depth(tree->parent) + 1;
 
 	if (!tree->parent)
 		return (depth);
