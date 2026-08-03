0d6560e28a84045ccabf409dedd548e4c84b1dea
Aurylias <brycevermorel@gmail.com>
2026-08-03 15:36:09 +0200

    Remade conditions in task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,11 +12,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height, right_height;
 
-	if (!tree)
+	if (!tree || (!tree->left && !tree->right))
 		return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	return ((left_height > right_height ? left_height : right_height) + 1);
+	left_height = binary_tree_height(tree->left) + 1;
+	right_height = binary_tree_height(tree->right) + 1;
+	if (left_height > right_height)
+		return (left_height);
+	else
+		return (right_height);
 }
