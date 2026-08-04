bb6c6ce1d1101577f70df30e24182c616ca4a1a8
Aurylias <brycevermorel@gmail.com>
2026-08-04 13:16:52 +0200

    Fixed return

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -13,9 +13,11 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	if (!node || !node->parent || node->parent->parent)
 		return (NULL);
 
-	if (node->parent->parent->left == node->parent)
-		return (node->parent->right);
-	else if (node->parent->parent->right == node->parent)
+	if (node->parent->parent->left == node->parent &&
+		node->parent->parent->right)
+		return (node->parent->parent->right);
+	else if (node->parent->parent->right == node->parent &&
+		node->parent->parent->parent->left)
 		return (node->parent->left);
 	else
 		return (NULL);
