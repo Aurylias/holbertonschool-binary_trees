c64f2e5760ee086cbc0675667ccb4fadef48a1f0
Aurylias <brycevermorel@gmail.com>
2026-08-04 12:16:42 +0200

    Remade all math

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -25,8 +25,9 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	h = height(tree);
 	n = size(tree);
 
-	for (i = 1; i < h - 1; i++)
+	for (i = 1; i < (h + 1); i++)
 		power *= 2 * i;
+	power -= 1;
 
 	if (power == n)
 		return (1);
