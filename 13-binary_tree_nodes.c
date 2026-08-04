f66b945f57a35c28f400ce95302d1b2643f8fb5e
Aurylias <brycevermorel@gmail.com>
2026-08-04 10:00:27 +0200

    Fixed logic

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -15,7 +15,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	if (!tree->left || !tree->right)
+	if (tree->left || tree->right)
 		nodes = 1;
 
 	return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right)
