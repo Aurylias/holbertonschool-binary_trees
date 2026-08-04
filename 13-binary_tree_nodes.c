db3ef77f358874804530bb8e75e069aea766bc53
Aurylias <brycevermorel@gmail.com>
2026-08-04 09:46:26 +0200

    Changed logique for task 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -10,11 +10,13 @@
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
+	size_t nodes = 0;
+
 	if (!tree)
 		return (0);
 
 	if (!tree->left || !tree->right)
-		return (0);
+		nodes = 1;
 
 	return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1);
 }
