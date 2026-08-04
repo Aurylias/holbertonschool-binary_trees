c01d7898f3bbc9f078ca7d13fb6664b7dd813213
Aurylias <brycevermorel@gmail.com>
2026-08-04 13:14:34 +0200

    Fixed logic

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -10,14 +10,12 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (!node || !node->parent)
+	if (!node || !node->parent || node->parent->parent)
 		return (NULL);
 
-	if (node->parent->parent->left == node->parent &&
-		node->parent->parent->right)
+	if (node->parent->parent->left == node->parent)
 		return (node->parent->right);
-	else if (node->parent->parent->right == node->parent &&
-		node->parent->parent->left)
+	else if (node->parent->parent->right == node->parent)
 		return (node->parent->left);
 	else
 		return (NULL);
