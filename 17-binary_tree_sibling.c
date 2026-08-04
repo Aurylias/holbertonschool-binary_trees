05db022c78ed42c7765186ff064db6dda08d79ec
Aurylias <brycevermorel@gmail.com>
2026-08-04 12:55:09 +0200

    Fixed logic

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -13,10 +13,10 @@ binary_tree_t * binary_tree_sibling( binary_tree_t *node)
 	if (!node || !node->parent)
 		return (NULL);
 
-	if (node->parent->left == node && !node->parent->right)
+	if (node->parent->left == node && node->parent->right)
 		return (node->parent->right);
-	else if (node->parent->right == node && !node->parent->left)
-			return (node->parent->left);
+	else if (node->parent->right == node && node->parent->left)
+		return (node->parent->left);
 	else
 		return (0);
 }
