c60270cfdcbd0a154f919f2446f5c226af34a648
Aurylias <brycevermorel@gmail.com>
2026-08-04 13:21:14 +0200

    A fixe again

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -15,8 +15,6 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 
 	if (node->parent->parent->left == node->parent)
 		return (node->parent->parent->right);
-	else if (node->parent->parent->left == node->parent)
-		return (node->parent->parent->left);
-	else
-		return (NULL);
+	return (node->parent->parent->left);
+
 }
