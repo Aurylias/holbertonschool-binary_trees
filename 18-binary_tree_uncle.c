7ebc4e8bd92f3b9f2f422516f088d77457b77e02
Aurylias <brycevermorel@gmail.com>
2026-08-04 13:20:19 +0200

    Changed first if

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -10,12 +10,12 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (!node || !node->parent || node->parent->parent)
+	if (!node || !node->parent || !node->parent->parent)
 		return (NULL);
 
 	if (node->parent->parent->left == node->parent)
 		return (node->parent->parent->right);
-	else if (node->parent->parent->right == node->parent)
+	else if (node->parent->parent->left == node->parent)
 		return (node->parent->parent->left);
 	else
 		return (NULL);
