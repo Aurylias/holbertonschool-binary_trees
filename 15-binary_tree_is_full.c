c00fe087d890893c5e8e73b2180858ad672de64e
Aurylias <brycevermorel@gmail.com>
2026-08-04 11:39:01 +0200

    Add the function nodes

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,5 +1,7 @@
 #include "binary_trees.h"
 
+size_t nodes(const binary_tree_t *tree);
+
 /**
 * binary_tree_is_full - Checks if a binary tree is full
 *
@@ -13,8 +15,30 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	if (binary_tree_nodes(tree) > 0)
+	if (nodes(tree) > 0)
 		return (0);
 	else
 		return (1);
+}
+
+/**
+* nodes - Counts the nodes with at least 1 child in a binary tree
+*
+* @tree: A pointer to the root of the binary tree
+*
+* Return: The number of nodes with at least 1 child
+*/
+
+size_t nodes(const binary_tree_t *tree)
+{
+	size_t nodes = 0;
+
+	if (!tree)
+		return (0);
+
+	if (tree->left || tree->right)
+		nodes = 1;
+
+	return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right)
+		+ nodes);
 }
