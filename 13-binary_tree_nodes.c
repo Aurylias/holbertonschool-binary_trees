d5ae5fac2cb9c552c9053acca3ecef6517da81c6
Aurylias <brycevermorel@gmail.com>
2026-08-04 09:59:19 +0200

    Fixed node

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -18,5 +18,6 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (!tree->left || !tree->right)
 		nodes = 1;
 
-	return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1);
+	return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right)
+		+ nodes);
 }
